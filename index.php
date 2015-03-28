6ce7c84581719c7d060a932b3a69de36c032b0e4
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-03-27 19:42:09 -0500

    Comienzo de proyecto Bigmarket

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
+<meta name="keywords" lang="es" content="">
+<meta name="robots" content="All">
+<meta name="description" lang="es" content="">
+<title>BIGMARKET.COM MAYORISTA</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/stylesheet.css" />
+<link rel="stylesheet" href="css/style1.css" />
+<link rel="stylesheet" type="text/css" href="css/style-menu.css">
+<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+</head>
+<body>
+<header>
+<div id="lado1"><a href="/"><h2>Bigmarket.com</h2></a></div>
+<div id="lado2"><?php include("menu.html"); ?></div>
+</header>
+
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/script-menu.js"></script>
+</body>
+</html>
