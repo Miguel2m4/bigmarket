36988c785ed9158486cae338bf27d46ac5ea7bf9
Miguel Gutierrez <miguel.gutierrez@inngeniate.com>
2015-04-06 18:00:26 -0500

    Carrito

---
+++ resultado-busqueda.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width , initial-scale=1 ,maximum-scale=1 user-scalable=no" />
+<meta name="keywords" lang="es" content="">
+<meta name="robots" content="All">
+<meta name="description" lang="es" content="">
+<title>Productos | BIGMARKET.COM MAYORISTA</title>
+<link rel="stylesheet" href="css/normalize.css" />
+<link rel="stylesheet" href="css/stylesheet.css" />
+<link rel="stylesheet" href="css/style1.css" />
+<link rel="stylesheet" type="text/css" href="css/style-menu.css">
+<script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+</head>
+<body>
+<header>
+<div id="lado1"><a href="/"><h2>Bigmarket.com</h2></a></div>
+<div id="lado2"><?php include("menu.html"); ?></div>
+</header>
+<div class="principal">
+<div id="busca">
+	<form>
+		<input type="text" placeholder="¿Qué deseas buscar?">
+	</form>
+</div>
+<div class="carrito">
+	<p>muestra lo quelleva el carrito</p>
+	<p>muestra lo quelleva el carrito</p>
+</div>
+
+<div id="lado-menu">
+<div id="lateral">
+<h2>Categorias</h2>
+<ul>
+	<a href=""><li>Viveres y Abarrotes</li></a>
+	<a href=""><li>Carnicos</li></a>
+	<a href=""><li>Pollo</li></a>
+	<a href=""><li>Pescaderia</li></a>
+	<a href=""><li>Salsamentaria</li></a>
+	<a href=""><li>Aseo industrial</li></a>
+	<a href=""><li>Rancho y Licores</li></a>
+	<a href=""><li>Todas</li></a>
+</ul>
+</div>	
+</div>
+<div id="lado-bus">
+<div id="tags">
+	<a href="">Salsamentaria <span class="icon-borrar"></span></a> <a href=""> Carnicos <span class="icon-borrar"></span></a> <a href="">Pollo <span class="icon-borrar"></span></a>
+</div>
+<!-- despliega productos -->
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+<div class="produ">
+	<div class="imagen-pro">
+	</div>
+	<div class="contenido-pro">
+		<h2>Salsa Tartara - Heinz</h2>
+		<p> <input type="number" placeholder="Cant"> Paquete de 12 Unds</p>
+		<h3>$ 12.500</h3>
+		<p><a href="">Añadir al carrito</a></p>
+		<br>
+	</div>
+</div>
+
+</div>
+
+
+</div>
+
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="js/script-menu.js"></script>
+
+</body>
+</html>
